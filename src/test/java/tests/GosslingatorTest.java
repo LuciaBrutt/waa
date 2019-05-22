4d822863831550f8e3c8d7086f97a8d84f8d0dfc
LuciaBrutt <bruttovszka@gmail.com>
2019-05-22 15:52:51 +0200

    refatorizacia pomocou metod Ryan

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -36,12 +36,9 @@ public class GosslingatorTest extends TestBase {
         //driver.findElement(By.id("addRyan")).click();
         //driver.findElement(By.id("removeRyan")).click();
         //3.overit pocitanie Ryanov
-        Assert.assertEquals("1", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("1", getRyanCounterNumber());
 
         // vypisem si do logu aktualny pocet z pocitadla ryanov
-        System.out.println("Number of ryans: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
-
-
         Assert.assertEquals("ryan", getCounterDescription ());
 
     }
@@ -60,8 +57,6 @@ public class GosslingatorTest extends TestBase {
 
         //2. kliknut na tlacidlo pridat
 
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
-
         addRyan();
         addRyan();
         addRyan();
@@ -72,11 +67,8 @@ public class GosslingatorTest extends TestBase {
         driver.findElement(By.id("removeRyan")).click();
         //3.overit pocitanie Ryanov
 
-        actualNumberOfRyans = getRyanCounterNumber();
-        String actualRyanDescription = getCounterDescription ();
-
-        Assert.assertEquals("2", actualNumberOfRyans);
-        Assert.assertEquals("ryans", actualRyanDescription);
+        Assert.assertEquals("2", getRyanCounterNumber());
+        Assert.assertEquals("ryans", getCounterDescription());
 
     }
 
@@ -86,31 +78,29 @@ public class GosslingatorTest extends TestBase {
 
         //toto raz bude cyklus
 
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
-
         for (int i = 0; i < 50; i++) {
-            //addRyanButton.click();
             addRyan();
-            String actualNumberOfRyans = getRyanCounterNumber();
             //porovnam skutocnu hodnotu zo stranky s hodnotou indexu +1
             //index si musim premenit na String aby som ich mohla porovnat
-            Assert.assertEquals(String.valueOf(i + 1), actualNumberOfRyans);
+            Assert.assertEquals(String.valueOf(i + 1), getRyanCounterNumber());
 
             //overit sklonovanie pomocou podmienky
-            String actualDescription = getCounterDescription ();
             if (i + 1 == 1) {
-                Assert.assertEquals("ryan", actualDescription);
+                Assert.assertEquals("ryan", getCounterDescription());
             }
 
             if (i + 1 >= 2) {
-                Assert.assertEquals("ryans", actualDescription);
+                Assert.assertEquals("ryans", getCounterDescription());
             }
 
+            // overim pocet obrazkov ryana
+
+            Assert.assertEquals(i+1, getNumberOfRyanImages());
+
             System.out.println("index i = " + i);
-            System.out.println("pocet ryanov = " + actualNumberOfRyans);
+            System.out.println("pocet ryanov = " + getRyanCounterNumber());
         }
         //alt + J oznacenie kazdeho dalsieho vyskytu
-
 
         Assert.assertEquals("NUMBER OF\n" +
                 "RYANS\n" +
@@ -129,16 +119,14 @@ public class GosslingatorTest extends TestBase {
         int clicksLimit = 30;
         int clicks = 0;
 
-        while (!actualNumberOfRyans.equals("50") || clicks < clicksLimit) {
+        while (!getRyanCounterNumber().equals("50") || clicks < clicksLimit) {
             //addRyanButton.click();
             addRyan();
-            actualNumberOfRyans = getRyanCounterNumber();
             clicks++;
-
-
             //System.out.println("ahoj");
         }
     }
+
     @Test
         public void itShouldDisplayNoRyanOnPageOpen(){
         Assert.assertEquals( 0,
@@ -148,8 +136,8 @@ public class GosslingatorTest extends TestBase {
 
     private void addRyan (){
         driver.findElement(By.id("addRyan")).click();
-
     }
+
     private String getRyanCounterNumber(){
         return driver.findElement(By.id("ryanCounter")).getText();
     }
