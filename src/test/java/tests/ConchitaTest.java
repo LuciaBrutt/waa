26ba9f21fe427ae327cb873a7aa7c90195f0d769
LuciaBrutt <bruttovszka@gmail.com>
2019-05-27 15:25:39 +0200

    posledny stav ConchitaTest pred du

---
+++ src/test/java/tests/ConchitaTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import base.TestBase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class ConchitaTest extends TestBase {
+
+    @Before
+    public void openPage() {
+
+        //1.otvorit stranku
+        driver.get(BASE_URL + "/zenaalebomuz.php");
+    }
+
+    @Test
+    public void noOptionShouldBeSelected() {
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='wurst']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Zena']/input")).isSelected());
+
+    }
+
+    @Test
+    public void itShouldSelectMale() {
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        //overit hlasku
+        String expectedMessage = "It's wurst";
+        String actualMessage = driver.findElement(By.cssSelector("h1.description")).getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+        //overit ze moznost zena nie je vybrata
+        Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Zena']/input")).isSelected());
+    }
+
+    @Test
+    public void imageShouldBeDisplayed() {
+        Assert.assertTrue(driver.findElement(By.cssSelector("img")).isDisplayed());
+
+    }
+
+
+    }
+
+
