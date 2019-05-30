9ff78b5b54800c907848b13c549d76f21deec172
LuciaBrutt <bruttovszka@gmail.com>
2019-05-30 18:42:24 +0200

    opak ConchitaTestOpak du 0605

---
+++ src/test/java/opakovanieTesty/ConchitaTestOpakovanie.java
@@ -0,0 +1,40 @@
+package opakovanieTesty;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ConchitaTestOpakovanie {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        //0.spustit prehliadac
+        driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost/zenaalebomuz.php");
+    }
+
+    @Test
+    public void noOptionShouldBeSelected() {
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='wurst']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Zena']/input")).isSelected());
+    }
+
+    @Test
+    public void itShouldSelectMale() {
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        //overit hlasku
+        String expectedMessage = "It's wurst";
+        String actualMessage = driver.findElement(By.cssSelector("h1.description")).getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+        //overit ze moznost zena nie je vybrata
+        Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Zena']/input")).isSelected());
+    }
+
+}
